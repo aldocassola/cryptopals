b6b36083cb6922fff3acd950b0d0516ff72c18d5
Aldo Cassola <acassola@usfq.edu.ec>
2017-11-15 23:12:05 -0500

    add testfiles and more problems

--- set1.go
+++ set1.go
@@ -1,20 +1,21 @@
 package cryptopals
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 )
 
 func hexToBase64(in string) string {
-	hs, error := hex.DecodeString(in)
+	return base64.StdEncoding.EncodeToString(hexDecode(in))
+}
 
-	if error != nil {
-		_ = fmt.Errorf("Invalid hex string: %s", in)
-		return ""
+func hexDecode(hs string) []byte {
+	res, err := hex.DecodeString(hs)
+	if err != nil {
+		panic("hexDecode: invalid hex string")
 	}
-
-	return base64.StdEncoding.EncodeToString(hs)
+	return res
 }
 
 func xor(s, t []byte) []byte {
@@ -28,7 +29,45 @@ func xor(s, t []byte) []byte {
 	for i := range s {
 		res[i] = s[i] ^ t[i]
 	}
-
 	return res
+}
 
+type langmap map[rune]float64
+
+func makeLangMap(text string) langmap {
+	lmap := make(langmap)
+
+	for _, r := range text {
+		lmap[r]++
+	}
+
+	for r := range lmap {
+		lmap[r] /= float64(len(text))
+	}
+
+	return lmap
+}
+
+func scoreLanguage(text string, lmap langmap) float64 {
+	result := float64(0)
+	for _, r := range text {
+		result += lmap[r]
+	}
+
+	return result / float64(len(text))
+}
+
+func findSingleKeyXor(ctbytes []byte, lmap langmap) (key byte, pt string, highest float64) {
+	for testKey := 0; testKey < 256; testKey++ {
+		keyBuf := bytes.Repeat([]byte{byte(testKey)}, len(ctbytes))
+		testpt := string(xor(ctbytes, keyBuf))
+
+		curScore := scoreLanguage(testpt, lmap)
+		if curScore > highest {
+			highest = curScore
+			key = byte(testKey)
+			pt = testpt
+		}
+	}
+	return
 }
--- set1_test.go
+++ set1_test.go
@@ -1,8 +1,12 @@
 package cryptopals
 
-import "testing"
-import "encoding/hex"
-import "bytes"
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"strings"
+	"testing"
+)
 
 func TestProblem1(t *testing.T) {
 	hs := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -14,12 +18,47 @@ func TestProblem1(t *testing.T) {
 }
 
 func TestProblem2(t *testing.T) {
-	s1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
-	s2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	s1 := hexDecode("1c0111001f010100061a024b53535009181c")
+	s2 := hexDecode("686974207468652062756c6c277320657965")
 	res := xor(s1, s2)
-	expected, _ := hex.DecodeString("746865206b696420646f6e277420706c6179")
+	expected := hexDecode("746865206b696420646f6e277420706c6179")
 
 	if !bytes.Equal(res, expected) {
 		t.Error("wrong xor: ", res, expected)
 	}
+}
+
+func readFile(filename string) string {
+	data, _ := ioutil.ReadFile(filename)
+	return string(data)
+}
+
+var englishMap = makeLangMap(readFile("testdata/warandpeace.txt"))
+
+func TestProblem3(t *testing.T) {
+	ctbytes := hexDecode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	key, pt, _ := findSingleKeyXor(ctbytes, englishMap)
+	log.Println("Found plaintext: ", pt)
+	log.Println("Key: ", key)
+}
+
+func TestProblem4(t *testing.T) {
+	data := readFile("testdata/4.txt")
+	lines := strings.Split(data, "\n")
+
+	highest := float64(0)
+	pt := ""
+	linenum := int(0)
+	for i, ln := range lines {
+		_, testpt, testscore := findSingleKeyXor(hexDecode(ln), englishMap)
+
+		if testscore > highest {
+			pt = testpt
+			highest = testscore
+			linenum = i
+		}
+	}
+
+	log.Printf("Detected single xor line: %d", linenum)
+	log.Printf("plaintext: %s", pt)
 }
