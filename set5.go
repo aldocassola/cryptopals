a083b9c850587911549667ad41149d4447ff3331
Aldo Cassola <acassola@usfq.edu.ec>
2017-12-08 08:34:16 -0500

    set 5

--- set5.go
+++ set5.go
@@ -194,16 +194,8 @@ func dhEchoTestClient(hostname string, port int, g, p *big.Int, numTests int, t 
 
 //RunDHEchoClient runs dhEcho client with given args
 func RunDHEchoClient(hostname string, port int) {
-	nistPstrs := strings.Fields(`ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
-e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
-3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
-6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
-24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
-c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
-bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
-fffffffffffff`)
 	var nistPstr string
-	for _, v := range nistPstrs {
+	for _, v := range strings.Fields(nistPstrs) {
 		nistPstr += v
 	}
 	nistP := bytesToBigInt(hexDecode(nistPstr))
